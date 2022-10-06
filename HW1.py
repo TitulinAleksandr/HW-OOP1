632b798ce4966ce13a4da49e06a646a2732571dc
Titulin Alexander <tito-1984@mail.ru>
2022-10-06 10:37:02 +0300

    Merge branch 'main'

--- HW1.py
+++ HW1.py
@@ -1,3 +1,4 @@
+from re import S
 import sys
 class Student:
     def __init__(self, name, surname, gender):
@@ -7,8 +8,10 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-    def add_courses(self, course_name):
+    def add_fin_courses(self, course_name):
         self.finished_courses.append(course_name)   
+    def add_prog_courses(self, course_name):
+        self.courses_in_progress.append(course_name)
     def rate_lecture(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
@@ -77,28 +80,71 @@ class Reviewer(Mentor):
     def __str__(self):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
-student = Student('Ruoy', 'Eman', 'Man')
-student.finished_courses += ['Git']
-student.courses_in_progress += ['Python']
-student.courses_in_progress += ['GH']
-student.grades['Git'] = [10, 10, 10, 10, 10]
-student.grades['Python'] = [10, 10]
+student1 = Student('Ben', 'Smit', 'man')
+student1.add_prog_courses('Python')
+student1.add_prog_courses('Git')
+student1.add_fin_courses('Введение в программирование')
+student2 = Student('Gina', 'Adamson', 'woman')
+student2.add_prog_courses('JavaScript')
+student2.add_prog_courses('html')
+student2.add_fin_courses('Английский для программистов')
+lecturer1 = Lecturer('Василий', 'Иванов')
+lecturer1.add_courses('Python')
+lecturer1.add_courses('Git')
+lecturer2 = Lecturer('Иван', 'Абрамов')
+lecturer2.add_courses('JavaScript')
+lecturer2.add_courses('html')
+reviewer1 = Reviewer('Анна', 'Петрова')
+reviewer1.add_courses('Python')
+reviewer1.add_courses('Git')
+reviewer2 = Reviewer('Екатерина', 'Кротова')
+reviewer2.add_courses('JavaScript')
+reviewer2.add_courses('html')
+reviewer1.rate_hw(student1, 'Python', 7)
+reviewer1.rate_hw(student1, 'Git', 8)
+reviewer1.rate_hw(student1, 'Python', 10)
+reviewer1.rate_hw(student1, 'Git', 4)
+reviewer2.rate_hw(student2, 'JavaScript', 6)
+reviewer2.rate_hw(student2, 'html', 10)
+reviewer2.rate_hw(student2, 'JavaScript', 9)
+reviewer2.rate_hw(student2, 'html', 7)
+student1.rate_lecture(lecturer1, 'Python', 10)
+student1.rate_lecture(lecturer1, 'Python', 7)
+student1.rate_lecture(lecturer1, 'Git', 8)
+student1.rate_lecture(lecturer1, 'Git', 10)
+student2.rate_lecture(lecturer2, 'JavaScript', 9)
+student2.rate_lecture(lecturer2, 'JavaScript', 6)
+student2.rate_lecture(lecturer2, 'html', 10)
+student2.rate_lecture(lecturer2, 'html', 7)
+# print(student1.stud_av())
+# print(student2.stud_av())
+# print(student1)
+# print(student2)
+# print(lecturer1)
+# print(lecturer2)
+# print(reviewer1)
+# print(reviewer2)
+# print(student1 < lecturer1)
+# print(student2 > lecturer2)
+students_list = [student1, student2]
+def grade_av(students_list, course):
+    sum = 0
+    count = 0
+    for x in students_list:
+        for y in x.grades[course]:
+            sum += y
+            count += 1
+    return round(sum/count, 1)
 
-cool_mentor = Lecturer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-cool_mentor.courses_attached += ['GH']
-mentor = Reviewer('Alex', 'Smith')
-mentor.courses_attached += ['Python']
-mentor.courses_attached += ['GH']
-student.rate_lecture(cool_mentor, 'Python', 4)
-student.rate_lecture(cool_mentor, 'GH', 7)
-mentor.rate_hw(student, 'Python', 7)
-mentor.rate_hw(student, 'Python', 4)
-mentor.rate_hw(student, 'Python', 9)
-mentor.rate_hw(student, 'Python', 1)
-student.rate_lecture(cool_mentor, 'Python', 8)
-student.rate_lecture(cool_mentor, 'GH', 9)
+lecturers_list = [lecturer1, lecturer2]
+def rating_al(lecturers_list, course):
+    sum = 0
+    count = 0
+    for x in lecturers_list:
+        for y in x.grades1[course]:
+            sum += y
+            count += 1
+    return round(sum/count, 1)
 
-
-
-
+print(grade_av(students_list, 'Python'))
+print(rating_al(lecturers_list, 'Python'))
